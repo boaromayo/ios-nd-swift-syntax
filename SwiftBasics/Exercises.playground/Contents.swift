b920bdbdb7380dde7a8529c74268fecb54bc47fa
Nico Poblete <nlypoblet@gmail.com>
2018-06-07 14:03:44 -0700

    Save exercises here

--- SwiftBasics/Exercises.playground/Contents.swift
+++ SwiftBasics/Exercises.playground/Contents.swift
@@ -12,23 +12,31 @@ let index2 = Int(arc4random() % 9)
 let sentence = "The \(nounArray[6]) spilled her \(nounArray[7])."
 let  sillySentence = "The \(nounArray[index1]) jumped over the \(nounArray[index2])."
 //: Now try it yourself! Declare a new string that incorporates objects from the noun array above. Write one sentence that makes sense and one "Madlib" sentence with randomly chosen words. Feel free to add words to the noun array or declare a new array.
-let yourSentence = "TODO: Incorporate objects from the noun array here."
-let yourSillySentence = "TODO: Incorporate randomly chosen objects from the noun array here."
+let yourSentence = "The \(nounArray[0]) barked at the \(nounArray[3])"
+let yourSillySentence = "The erratic \(nounArray[index1]) wasn't too happy when the \(nounArray[index2]) came crashing down."
 //: ### Exercise 2
 //: Recreate the shoutString by using the didYouKnowString as a stem.
 let didYouKnowString = "Did you know that the Swift String class comes with lots of useful methods?"
 let whisperString = "psst" + ", " + didYouKnowString.lowercased()
-let shoutString =  "HEY! DID YOU KNOW THAT THE SWIFT STRING CLASS COMES WITH LOTS OF USEFUL METHODS?"
+let shoutString =  "HEY!" + didYouKnowString.uppercased()
 //: ### Exercise 3
 //: How many characters are in this string? 
 let howManyCharacters = "How much wood could a woodchuck chuck if a woodchuck could chuck wood?"
+print ("There are " + \(howManyCharacters.count) + " characters.")
 //: ### Exercise 4
 //: How many times does the letter "g" or "G" appear in the following string? Use a for-in loop to find out!
 let gString = "Gary's giraffe gobbled gooseberries greedily"
 var count = 0
+for (character in gString) {
+  if (character == "G" || character == "g") {
+    count += 1 
+  }
+}
 //: ### Exercise 5
 //: Write a program that tells you whether or not this string contains the substring "tuna".
 let word = "fortunate"
+let index = word.firstIndex(of: "t") ?? word.firstIndex(of: "a")
+print(index 
 //: ### Exercise 6
 //: Write a program that deletes all occurrences of the word "like" in the following string.
 let lottaLikes = "If like, you wanna learn Swift, like, you should build lots of small apps, cuz it's like, a good way to practice."
